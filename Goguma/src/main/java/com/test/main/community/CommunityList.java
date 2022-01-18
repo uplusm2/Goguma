41e9ccb21344757a2f3914189eb2b6727494bd3a
umingming <u_0112@naver.com>
2022-01-18 18:13:28 +0900

    Add coummuityList

---
+++ Goguma/src/main/java/com/test/main/community/CommunityList.java
@@ -0,0 +1,21 @@
+package com.test.main.community;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/community/communityList.do")
+public class CommunityList extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/community/communityList.jsp");
+		dispatcher.forward(req, resp);
+	}
+}
---
+++ Goguma/src/main/webapp/WEB-INF/views/community/communityList.jsp
@@ -0,0 +1,28 @@
+<%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Code</title>
+<%@ include file="/WEB-INF/views/inc/asset.jsp" %>
+<style>
+
+</style>
+</head>
+<body>
+	<!-- main.jsp -->
+	<main class="main">
+		<%@include file="/WEB-INF/views/inc/header.jsp" %>
+		<section class="content">
+			<div class="title">
+				<h2>커뮤니티</h2>
+			</div>
+		</section>
+		<%@include file="/WEB-INF/views/inc/footer.jsp" %>
+	</main>
+	
+	<script>
+	
+	</script>
+</body>
+</html>
