739de4f37b52c29bd63aaaec72147e504cd18f70
hyein8290 <90780701+hyein8290@users.noreply.github.com>
2022-01-21 16:03:58 +0900

    Add files via upload

---
+++ Goguma/src/main/java/com/test/main/admin/UserDTO.java
@@ -0,0 +1,81 @@
+package com.test.main.admin;
+
+//TODO 다른 분 userdto 써야 되나?
+public class UserDTO {
+	
+	private String id;
+	private String name;
+	private String nickname;
+	private String tel;
+	private String email;
+	private String address;
+	private String birth;
+	private String since;
+	private double score;
+	private String state;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getSince() {
+		return since;
+	}
+	public void setSince(String since) {
+		this.since = since;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	
+	
+	
+}
