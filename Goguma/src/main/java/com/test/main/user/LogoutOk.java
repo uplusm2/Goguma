66c7c5b1090c1d04391b180e8e61b05424f0322c
whyalwaysm <93183559+whyalwaysm@users.noreply.github.com>
2022-01-21 14:55:27 +0900

    Update LogoutOk.java

--- Goguma/src/main/java/com/test/main/user/LogoutOk.java
+++ Goguma/src/main/java/com/test/main/user/LogoutOk.java
@@ -1,4 +1,4 @@
-package com.test.main.user;
+package com.test.main.member;
 
 import java.io.IOException;
 
@@ -10,25 +10,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/member/logoutok.do")
+@WebServlet("/user/logoutok.do")
 public class LogoutOk extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		//할일
-		//1. 인증 티켓 제거
+
 		
 		HttpSession session = req.getSession();
 		
-		//session.removeAttribute("id");
-		//session.removeAttribute("name");
-		//session.removeAttribute("lv");
+
 		
 		session.invalidate(); //주의!!
 		
 		
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/member/logoutok.jsp");
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/user/logoutok.jsp");
 		dispatcher.forward(req, resp);
 	}
 
