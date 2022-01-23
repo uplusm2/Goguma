907bfceb17ba8933bba85d9633085c2def937557
whyalwaysm <93183559+whyalwaysm@users.noreply.github.com>
2022-01-23 20:30:24 +0900

    Update LoginOk.java
    하드업뎃

--- Goguma/src/main/java/com/test/main/user/LoginOk.java
+++ Goguma/src/main/java/com/test/main/user/LoginOk.java
@@ -1,6 +1,7 @@
 package com.test.main.user;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,22 +11,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.test.main.user.UserDAO;
+import com.test.main.user.UserDTO;
+
 @WebServlet("/user/loginok.do")
 public class LoginOk extends HttpServlet {
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 
-		
 		//1.
 		String id = req.getParameter("id");
 		String pw = req.getParameter("pw");
 		
 		//2.
 		UserDAO dao = new UserDAO();
-		
-
 		
 		UserDTO dto = new UserDTO();
 		dto.setId(id);
@@ -35,21 +36,23 @@ public class LoginOk extends HttpServlet {
 		
 		if (result != null) {
 			
-			
+			//로그인 성공
 			HttpSession session = req.getSession();
 			
-			session.setAttribute("id", result.getId()); 
-			session.setAttribute("name", result.getName());
-			session.setAttribute("lv", result.getLv());
-						
+			session.setAttribute("id", result.getId()); //인증 티켓
+			
+			RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/user/loginok.jsp");
+			dispatcher.forward(req, resp);
+		
+		} else {
+			PrintWriter script = resp.getWriter();
+			script.println("<script>");
+			script.println("alert('ID or PW be wrong')");
+			script.println("history.back();");
+			script.println("</script>");
+			
 		}
 
-		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/user/loginok.jsp");
-		dispatcher.forward(req, resp);
 	}
-
+	
 }
-
-
-
-
