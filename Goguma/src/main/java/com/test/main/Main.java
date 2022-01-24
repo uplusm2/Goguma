1a9d1d73252b536ea8bfa27b34453598dd838b71
hyein8290 <hyein8290@gmail.com>
2022-01-24 23:24:59 +0900

    Feat: Add req encoding UTF-8

--- Goguma/src/main/java/com/test/main/Main.java
+++ Goguma/src/main/java/com/test/main/Main.java
@@ -15,6 +15,8 @@ public class Main extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		req.setCharacterEncoding("UTF-8");
+		
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/main.jsp");
 		dispatcher.forward(req, resp);
 	}
