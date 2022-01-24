229eeb592476382a53b649780edc659c6c9eda34
Hwang-97 <qqq4472@gmail.com>
2022-01-25 00:15:42 +0900

    feat:Update ProfileDAO.java

--- Goguma/src/main/java/com/test/main/profile/ProfileDAO.java
+++ Goguma/src/main/java/com/test/main/profile/ProfileDAO.java
@@ -237,7 +237,7 @@ public class ProfileDAO {
 	public ArrayList<TransactionRecordDTO> getSalesRecord(HashMap<String, String> map) {
 		ArrayList<TransactionRecordDTO> list = new ArrayList<TransactionRecordDTO>();
 		String sql = "select * from(select a.* , rownum as rnum from( select * from (vwproductsold p left outer join tblreview re on p.DEAL_SEQ = re.deal_seq)\r\n"
-				+ "        where id = ? and type='B' order by p.regdate) a) where rnum between ? and ?";
+					+ "where id = ? order by p.regdate) a) where rnum between ? and ? -- 판매한것";
 		try {
 			
 			pstat = conn.prepareStatement(sql);
@@ -272,8 +272,8 @@ public class ProfileDAO {
 	public int getSalesRecordTotalPage(HashMap<String, String> map) {
 		String sql = "select -- 판매한것\r\n"
 				+ "    count(*) as cnt\r\n"
-				+ "from(select a.*,rownum as rnum from(select * from vwproductsold where id = ? order by regdate) a) a\r\n"
-				+ "            left outer join tblreview re on a.DEAL_SEQ = re.deal_seq where re.type='B'";
+				+ " from(select a.*,rownum as rnum from(select * from vwproductsold where id = ? order by regdate) a) a\r\n"
+				+ "             left outer join tblreview re on a.DEAL_SEQ = re.deal_seq";
 		try {
 			pstat = conn.prepareStatement(sql);
 			pstat.setString(1, map.get("id"));
