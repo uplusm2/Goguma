fd50f9260d965b5a90f611c06b1677c9d989e524
chanwoo lee <lcw7407@naver.com>
2022-01-25 17:01:48 +0900

    update : main import 삭제

--- Goguma/src/main/java/com/test/main/Main.java
+++ Goguma/src/main/java/com/test/main/Main.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.test.main.profile.ProfileEditOk;
-
 @WebServlet("/main.do")
 public class Main extends HttpServlet {
 
