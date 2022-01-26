51bc27b157c72a3fb49ca955a40db80b686c6ead
umingming <u_0112@naver.com>
2022-01-26 19:28:18 +0900

    Chore: Update DBUtil parameter

--- Goguma/src/main/java/com/test/main/community/ComCommentDAO.java
+++ Goguma/src/main/java/com/test/main/community/ComCommentDAO.java
@@ -19,8 +19,8 @@ public class ComCommentDAO {
 	
 	public ComCommentDAO() {
 		try {
-			conn = DBUtil.open();
-			//conn = DBUtil.open("goguma", "java1234");
+//			conn = DBUtil.open();
+			conn = DBUtil.open("goguma", "java1234");
 			stat = conn.createStatement();
 		} catch (Exception e) {
 			e.printStackTrace();
