5a47c661b7180b2361fe46ee19921e814063d5ac
Hwang-97 <qqq4472@gmail.com>
2022-01-27 21:27:57 +0900

    Feat : Add ProductRegistrationok.java

--- Goguma/src/main/java/com/test/main/product/ProductRegistrationok.java
+++ Goguma/src/main/java/com/test/main/product/ProductRegistrationok.java
@@ -15,57 +15,61 @@ import com.test.main.profile.ProfileDAO;
 
 @WebServlet("/product/productRegistrationok.do")
 public class ProductRegistrationok extends HttpServlet {
+//	private String 
+//	pathDirs
+//	content
+//	productName
+//	price
+//	type
+//	"category"
+//	sido
+//	sgg
+//	emd
 	
-
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		ProfileDAO dao  = new ProfileDAO();
 		 try {
 	         
-	         //Request -> (대신 + 파일 업로드) -> com.reilly.servlet.MultipartRequest
-	         //리퀘스트를 이제 멀티가 대신할 것
-	         //근데 리퀘스트가 하는게 getParameter랑 인코딩밖에 없음(인코딩은 리퀘스트가 함)
-	         //즉 getParamet만 하면 됨.
-	         
-	         //매개변수 = (리퀘스트, 업로드할 파일위치, 파일 크기 제한, 인코딩, 디폴트)
-	         
-	         //MultipartRequest 객체 생성 순간 파일 업로드 처리도 같이 완료!!
 			 
 			 MultipartRequest  multi = new MultipartRequest(
 	                                 req,
-	                                 "C:\\Goguma\\Goguma\\src\\main\\webapp\\files\\profile",
+	                                 "C:\\Goguma\\Goguma\\src\\main\\webapp\\files\\product",
 	                                 1024 * 1024 * 100,
 	                                 "UTF-8",
 	                                 new DefaultFileRenamePolicy()
 	                            );
-	         String id = (String)req.getSession().getAttribute("id");
-	         String nickName = multi.getParameter("nickName");
-	         String intro = multi.getParameter("intro");
-	         
-	         String path = multi.getFilesystemName("pathDir");
-	         
-	         if(nickName==null) nickName = (String)req.getSession().getAttribute("nickname");
-	         if(nickName==null) intro = " ";
-	         if(nickName==null) path = "default image.jpg";
-	         
-	         
-	         HashMap<String,String> map = new HashMap<String,String>();
-	         
-	         map.put("id", id);
-	         map.put("intro", intro);
-	         map.put("nickName", nickName);
-	         map.put("path", path);
-	         
-	         int check = dao.setProfile(map);
-	         
-	         System.out.println(check); // 나중에 처리
-	 		 System.out.println(path);
-	 		 System.out.println(id);
-	 		 
-	 		 req.setAttribute("check", check);
-	 		 req.setAttribute("nickName", nickName);
-	 		 
-	 		 req.getSession().setAttribute("nickname", nickName);
+			 
+			 System.out.println(multi.getFilesystemName("pathDirs[]"));
+			 
+//	         String id = (String)req.getSession().getAttribute("id");
+//	         String nickName = multi.getParameter("nickName");
+//	         String intro = multi.getParameter("intro");
+//	         
+//	         String path = multi.getFilesystemName("pathDir");
+//	         
+//	         if(nickName==null) nickName = (String)req.getSession().getAttribute("nickname");
+//	         if(nickName==null) intro = " ";
+//	         if(nickName==null) path = "default image.jpg";
+//	         
+//	         
+//	         HashMap<String,String> map = new HashMap<String,String>();
+//	         
+//	         map.put("id", id);
+//	         map.put("intro", intro);
+//	         map.put("nickName", nickName);
+//	         map.put("path", path);
+//	         
+//	         int check = dao.setProfile(map);
+//	         
+//	         System.out.println(check); // 나중에 처리
+//	 		 System.out.println(path);
+//	 		 System.out.println(id);
+//	 		 
+//	 		 req.setAttribute("check", check);
+//	 		 req.setAttribute("nickName", nickName);
+//	 		 
+//	 		 req.getSession().setAttribute("nickname", nickName);
 	 		 
 	      } catch (Exception e) {
 	         System.out.println("productRegistrationok.doPost()");
